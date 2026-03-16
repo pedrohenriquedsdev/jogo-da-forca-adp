c9c2fdfeba927f16617daad1864a585a3451131f
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-03-16 17:04:15 -0300

    Versão temporária do projeto

--- jogo-da-forca-adp.ConsoleApp/Program.cs
+++ jogo-da-forca-adp.ConsoleApp/Program.cs
@@ -1,9 +1,127 @@
-﻿namespace jogo_da_forca_adp.ConsoleApp;
+﻿using System.Security.Cryptography;
+
+namespace jogo_da_forca_adp.ConsoleApp;
+
+/*
+Especificação: 
+    Nosso próximo desafio é desenvolver um jogo de forca. O computador escolherá, de maneira
+    aleatória, uma palavra entre várias possibilidades, e o jogador deve chutar, letra por letra, até
+    adivinhá-la.
+    Se o jogador chutar 5 letras erradas, ele perde.
+
+Requisitos
+3. O jogador poderá cometer até cinco erros, caso erre pela quinta vez, ou acerte a palavra a partida
+acaba.
+4. Deve-se apresentar um desenho da forca sendo atualizado a cada erro.
+*/
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("=================");
+            Console.WriteLine("Jogo da forca");
+            Console.WriteLine("=================");
+
+            //LÓGICA DO JOGO DA FORCA 
+
+            // 1. Ao iniciar o jogo, deve ser selecionada uma palavra aleatória à partir de uma lista.
+            //Escolher uma palavra aleatória
+            string palavraAleatoria = EscolherPalavraAleatoria();
+
+            Console.WriteLine(palavraAleatoria);
+
+            // 2. O jogador poderá chutar a palavra secreta letra por letra, cada letra certa deverá ser apresentada, assim como as letras erradas.
+            char[] letrasAcertadas = new char[palavraAleatoria.Length];
+
+            for (int caractere = 0; caractere < letrasAcertadas.Length; caractere++)
+            {
+                letrasAcertadas[caractere] = '_';
+            }
+
+            bool jogadorAcertouPalavra = false;
+
+            while (jogadorAcertouPalavra == false)
+            {
+                Console.WriteLine(letrasAcertadas);
+
+                Console.Write("Digite uma letra: ");
+                string? strLetra = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(strLetra))
+                {
+                    Console.WriteLine("Digite uma letra ou um caractere válido!");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                char letraChute = Convert.ToChar(strLetra.ToUpper());
+
+                for (int contador = 0; contador < palavraAleatoria.Length; contador++)
+                {
+                    char letraAtual = palavraAleatoria[contador];
+
+                    if (letraChute == letraAtual)
+                    {
+                        letrasAcertadas[contador] = letraAtual;
+                    }
+                }
+
+                ogadorAcertouPalavra = palavraAleatoria == string.Join("", letrasAcertadas);
+            }
+
+            Console.WriteLine("Deseja continuar o jogo? (S/N): ");
+            string? opcaoContinuar = Console.ReadLine()?.ToUpper();
+
+            if (opcaoContinuar != "S")
+                break; //caimos fora do loop e do programa
+        }
+    }
+
+    static string EscolherPalavraAleatoria()
+    {
+        Console.WriteLine("Escolhendo palavra aleátoria");
+
+        string[] palavras = [
+           "ABACATE",
+            "ABACAXI",
+            "ACEROLA",
+            "AÇAÍ",
+            "ARAÇÁ",
+            "ABACATE",
+            "BACABA",
+            "BACURI",
+            "BANANA",
+            "CAJÁ",
+            "CAJU",
+            "CARAMBOLA",
+            "CUPUAÇU",
+            "GRAVIOLA",
+            "GOIABA",
+            "JABUTICABA",
+            "JENIPAPO",
+            "MAÇÃ",
+            "MANGABA",
+            "MANGA",
+            "MARACUJÁ",
+            "MURICI",
+            "PEQUI",
+            "PITANGA",
+            "PITAYA",
+            "SAPOTI",
+            "TANGERINA",
+            "UMBU",
+            "UVA",
+            "UVAIA"
+       ];
+
+        int indiceAleatorio = RandomNumberGenerator.GetInt32(palavras.Length); //APENAS 1 ARG PARA O RNG (GERA NUM ALEATÓRIO ENTRE 0 E O VARIAVEL.LENGTH) 
+
+        string palavraAleatoria = palavras[indiceAleatorio]; //RECEBE ÍNDICE ALEATÓRIO PARA ARMAZENAR ESSA PALAVRA ALEATÓRIA
+
+        return palavraAleatoria;
     }
 }
